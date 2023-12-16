24a7498719175337e7543b6ab6af2ce83534750f
pbbb <2429130100@qq.com>
2023-12-15 20:25:12 -0800

    add unit test

--- UnitTest.hs
+++ UnitTest.hs
@@ -2,6 +2,7 @@ module UnitTest where
 
 import Test.QuickCheck
 import Player
+import Game
 import Linear.V2 (V2 (..))
 import Control.Lens (makeLenses, (%~), (&), (.~), (^.), _1, _2)
 
@@ -14,3 +15,7 @@ checkCreateEnemy coord dir = case dir of
     Lft -> ((coord ^. _1) ==  0) && ((coord ^. _2) >= 0) && ((coord ^. _2) <=  gridHeight)
     Rt -> ((coord ^. _1) ==  gridWidth) && ((coord ^. _2) >= 0) && ((coord ^. _2) <=  gridHeight)
     otherwise -> False
+
+-- Check that all enemies in the list is alive and dead ones are removed
+prop_enemy_alive :: Game -> Bool
+prop_enemy_alive g = foldr (&&) True (map isEnemyAlive (enemies g))
